3ecf0c4d7fcc43e78c0258cb9bcb04ccfc96945a
kittisakdiw87-hub <kittisakdiw87@gmail.com>
2026-07-02 15:39:14 +0700

    add Total add .

--- week3/Total.java
+++ week3/Total.java
@@ -4,5 +4,5 @@ void main() {
     IO.print("Do you want second number : ");
     int number2 = Integer.parseInt(IO.readln());
     int total = number1 + number2;
-    IO.println("Total is : " + total);
+    IO.println("Total is. : " + total);
 }
